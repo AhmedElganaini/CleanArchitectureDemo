7680705a3dc6dba9f8ee16ae9912f7d7ef025061
Mohamed Sallam <mohamed.sallam@z2data.com>
2025-02-03 11:51:43 +0200

    comment

--- CleanArchitectureDemo/Controllers/ProductsController.cs
+++ CleanArchitectureDemo/Controllers/ProductsController.cs
@@ -24,6 +24,7 @@ namespace CleanArchitectureDemo.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreateProductDto dto)
         {
+            // create
             await _productService.CreateAsync(dto);
             return Ok();
         }
